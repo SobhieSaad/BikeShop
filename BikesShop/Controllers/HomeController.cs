9e0429230e3f71ffb47481cf39dfd7461b3c35f3
SobhieSaad <Sobhie.saad1@hotmail.com>
2019-01-27 18:05:51 +0200

    First commit

---
+++ BikesShop/Controllers/HomeController.cs
@@ -0,0 +1,215 @@
+﻿using BikesShop.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BikesShop.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private BikesOnlineShopDBEntities db = new BikesOnlineShopDBEntities();
+        public ActionResult Index()
+        {
+            var products = GetTopSellingProducts(3);
+
+            return View(products);
+        }
+        private List<Product> GetTopSellingProducts(int count)
+        {
+            // Group the order details by product and return
+            // the product with the highest count
+
+            return db.Products
+                .OrderByDescending(a => a.OrdersDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+
+        //to configure action that admin choose(edit,details,delete)
+
+        public ActionResult ChhoseActionProduct(int? Id, string Command1)
+        {
+
+            return RedirectToAction(Command1 + "Product", "Home", new { Id = Id });
+
+        }
+
+        [HttpGet]
+        public ActionResult EditProduct(int? Id)
+        {
+            var p = db.Products.Find(Id);
+            return View(p);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProduct(Product pro, string Command1)
+        {
+            if (Command1.Equals("Save") && Command1 != null)
+            {
+                db.Entry(pro).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("AdminIndex", "Home");
+            }
+            else
+            {
+                return RedirectToAction("AdminIndex", "Home");
+            }
+
+
+        }
+
+        public ActionResult DetailsProduct(int? id)
+        {
+            var pro = db.Products.Find(id);
+
+            return View(pro);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteProduct(int? id)
+        {
+            var p = db.Products.Find(id);
+            return View(p);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteProduct(int? Id, string Command)
+        {
+            if (Command.Equals("Delete"))
+            {
+                Product pro = db.Products.Find(Id);
+                db.Products.Remove(pro);
+                db.SaveChanges();
+                ViewData["RemovesSuccess"] = "Product (" + pro.Name + ") removed successfully";
+                return RedirectToAction("AdminIndex", "Home");
+            }
+            else
+            {
+                return RedirectToAction("AdminIndex", "Home");
+            }
+
+
+        }
+
+        public ActionResult CrerateProduct(string command)
+        {
+
+            return View();
+        }
+
+        //for admin to create new product
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddProduct(Product pro, string Command1)
+        {
+            if (Command1.Equals("Create") && Command1 != null)
+            {
+                if (db.Products.Find(pro.Id) == null)
+                {
+                    ViewBag.Exist = "Product (" + pro.Name + ") Already Exist..";
+                }
+                db.Products.Add(pro);
+                db.SaveChanges();
+                return RedirectToAction("AdminIndex", "Home");
+            }
+            else
+            {
+                return RedirectToAction("AdminIndex", "Home");
+            }
+        }
+
+        public ActionResult ShowProducts()
+        {
+
+            return View(db.Products.ToList());
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        //public ActionResult Statistics()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult AdminReports(DateReviewer d, string Command1)
+        //{
+        //    if (Command1.Equals("Produce Report"))
+
+        //        return RedirectToAction("ProduceReport", "Home",d);
+        //    else if (Command1.Equals("Draw Chart"))
+        //        return RedirectToAction("DrawChart", "Home");
+        //    else
+        //        return RedirectToAction("AdminIndex", "Home");
+        //}
+
+        public ActionResult ProduceReport(DateReviewer d)
+        {
+
+            var orderDates = (from orders in db.Orders.Where(o => o.OrderDate >= d.StartDate &&
+                          o.OrderDate <= d.EndDate)
+                              group orders by orders.OrderDate into ord
+
+
+                              select new
+                              {
+                                  OrderDate = ord.FirstOrDefault().OrderDate,
+                                  total = ord.FirstOrDefault().total
+
+                              }
+                        ).ToList();
+
+            ArrayList dates = new ArrayList();
+            ArrayList Totals = new ArrayList();
+            DateTime end = d.StartDate.Value.AddDays(orderDates.Count - 1);
+            while (d.StartDate <= end)
+            {
+
+
+                Totals.Add((from or in db.Orders
+                            where or.OrderDate.Value.Day == d.StartDate.Value.Day
+                            select or.total).Sum());
+
+                d.StartDate = d.StartDate.Value.AddDays(1);
+            }
+            orderDates.ToList().ForEach(od => dates.Add(od.OrderDate));
+
+            new System.Web.Helpers.Chart(width: 1000, height: 400).AddSeries
+                (chartType: "column", xValue: dates, yValues: Totals).Write("jpeg");
+
+            return null;
+        }
+
+        [HttpPost]
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Cancel()
+        {
+            return RedirectToAction("AdminIndex", "Home");
+        }
+
+    }
+}
---
+++ BikesShop/Models/ProductCommentRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikesShop.Models
+{
+    public class ProductCommentRate
+    {
+        public Product Product { get; set; }
+
+        public CustomersComment CustomersComment { get; set; }
+
+        public ProductRate ProductRate { get; set; }
+
+        public List<CustomersComment> ListComments { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<CustomersComment> CustssComments { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductRate> ProductRates { get; set; }
+    }
+}
